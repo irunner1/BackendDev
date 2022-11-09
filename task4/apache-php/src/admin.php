c8bd841b7a2d44155e4729491238d49aedc94fa9
Artem <aspuchkov02@gmail.com>
2022-11-09 11:34:26 +0300

    added table style

--- task4/apache-php/src/admin.php
+++ task4/apache-php/src/admin.php
@@ -4,7 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <title>Админ панель</title>
-    <link rel="stylesheet" href="../css/style.css" type="text/css" />
+    <link rel="stylesheet" href="/css/style.css" type="text/css" />
+    <style>
+        table{
+            border: 1px solid #eee;
+            table-layout: fixed;
+            width: 70%;
+            margin-bottom: 20px;
+        }
+        table th {
+            font-weight: bold;
+            padding: 5px;
+            background: #efefef;
+            border: 1px solid #dddddd;
+        }
+        table td{
+            padding: 5px 10px;
+            border: 1px solid #eee;
+            text-align: left;
+        }
+        table tbody tr:nth-child(odd){
+            background: #fff;
+        }
+        table tbody tr:nth-child(even){
+            background: #F7F7F7;
+        }
+    </style>
 </head>
 
 <body>
--- task4/apache-php/src/catalogue.php
+++ task4/apache-php/src/catalogue.php
@@ -5,6 +5,31 @@
     <meta charset="UTF-8">
     <title>Каталог</title>
     <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <style>
+        table{
+            border: 1px solid #eee;
+            table-layout: fixed;
+            width: 70%;
+            margin-bottom: 20px;
+        }
+        table th {
+            font-weight: bold;
+            padding: 5px;
+            background: #efefef;
+            border: 1px solid #dddddd;
+        }
+        table td{
+            padding: 5px 10px;
+            border: 1px solid #eee;
+            text-align: left;
+        }
+        table tbody tr:nth-child(odd){
+            background: #fff;
+        }
+        table tbody tr:nth-child(even){
+            background: #F7F7F7;
+        }
+    </style>
 </head>
 
 <body>
