4d25f0072f5fa0cd3b9620aa79a842fb0cbe3ced
Artem <aspuchkov02@gmail.com>
2022-11-21 17:43:52 +0300

    added pdf style

--- task5/apache-php/src/pdf/show_pdf.php
+++ task5/apache-php/src/pdf/show_pdf.php
@@ -12,36 +12,29 @@
     <main>
         <div class="block">
             <div class="text">Отправить файл</div>
-
+            <br>
             <form enctype="multipart/form-data" action="./upload_pdf.php" method="POST">
                 <div class="file-input">
                     <input name="userfile" type="file" id="file" class="file">
-                    <label for="file">Выберите файл</label>
+                    <label for="file">Выбрать файл</label>
                 </div>
                 <br>
-                <input type="submit" value="Отправить файл" />
-            </form>
-            <form enctype="multipart/form-data" action="./upload_pdf.php" method="POST">
-                <div>
-                    <label for="file_field">Отправить файл:</label>
-                    <br>
-                    <input name="userfile" type="file" />
+                <div class="file-input">
+                    <input  type="submit" id="submit" class="file">
+                    <label for="submit">Отправить файл</label>
                 </div>
-                <br>
-                <input type="submit" value="Отправить файл" />
             </form>
-
             <br>
             <div class="btn_link">
                 <a class="link" href="/index.php">На главную</a>
             </div>
-            <div class="text"> Файлы сервера </div>
-            <table cellspacing="0" , style="width:50%">
+            <div class="text"> Файлы на сервере </div>
+            <table cellspacing="0" , style="width:100%">
                 <?php
                     $scanned_directory = array_diff(scandir('./files'), array('..', '.'));
                     if (count($scanned_directory) > 0) {
                         foreach ($scanned_directory as $file) {
-                            echo "<tr><td><a class='filelink' href='./files/" . $file . "'>" . $file . "</a></td></tr>";
+                            echo "<tr><td><a class='table_link' href='./files/" . $file . "'>" . $file . "</a></td></tr>";
                         }
                     }
                 ?>
